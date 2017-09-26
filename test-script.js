fd88be4b91137e6147b5da68f7a017d659fa3637
arsalan-menhaj <arsalan.menhaj@gmail.com>
2017-09-26 15:01:45 +0000

    Initial commit - script accepts single argument as query within name fields

---
+++ test-script.js
@@ -0,0 +1,27 @@
+argument = process.argv[2]
+
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  })
+  .then(
+    client.query(`SELECT * FROM famous_people WHERE first_name = '${argument}' OR last_name = '${argument}'`, (err, result) => {
+      if (err) {
+        return console.error("error running query", err);
+      }
+    console.log(result.rows[0]); //output: 1
+    client.end();
+  });
+});
